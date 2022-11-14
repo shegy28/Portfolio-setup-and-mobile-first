27d0ae3553d64bb91ddf2188b9b21803d65ff06e
shegy28 <shegy28@gmail.com>
2022-11-14 18:45:45 +0200

    Fixed linter errors

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function openNav() {
-    document.getElementById("drop-down").style.width = "100%";
-    document.getElementById("container").style.filter = "blur(7px)";
-  }
-  
-  function closeNav() {
-    document.getElementById("drop-down").style.width = "0%";
-    document.getElementById("container").style.filter = "blur(0px)";
-  }
+  document.getElementById('drop-down').style.width = '100%';
+  document.getElementById('container').style.filter = 'blur(7px)';
+}
+
+function closeNav() {
+  document.getElementById('drop-down').style.width = '0%';
+  document.getElementById('container').style.filter = 'blur(0px)';
+}
