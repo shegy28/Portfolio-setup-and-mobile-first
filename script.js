82736d3a77838718be42d6acc26f2e1658b8d777
shegy28 <shegy28@gmail.com>
2022-11-14 19:10:23 +0200

    Fix linter error

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ function openNav() {
   document.getElementById('drop-down').style.width = '100%';
   document.getElementById('container').style.filter = 'blur(7px)';
 }
+openNav();
 
 function closeNav() {
   document.getElementById('drop-down').style.width = '0%';
   document.getElementById('container').style.filter = 'blur(0px)';
 }
+closeNav();
