b8d21bc714b12d1b8806e7cba83bb334622163ab
Lornakaboro <lornakaboro@gmail.com>
2022-11-16 21:12:48 +0300

    Add desktop pop-up section dynamically

--- script.js
+++ script.js
@@ -264,3 +264,98 @@ for (let i = 0; i < projectDetails.length; i += 1) {
 }
   /* End of project detail population when the pages loads */
 
+  function openPopup() {
+    document.querySelector('#popup-section').innerHTML += `<div class="popup-container">
+    <div class="popup"> 
+        <div id="desktop-popup" class="">
+            <div class="info-header popup-header">
+                <h4 class="tonic">Uber Navigation
+                </h4>
+                <img src="./images/popup=x-Icon.png" alt="popup-x icon" class="popup-header-image" onclick="closePopup()">
+            </div>
+            <div class="info-icons">
+                <h4 class="info-icons-p1">Uber</h4>
+                <img src="./images/Counter.png" alt="dot">
+                <h4 class="info-icons-p2">Lead Developer</h4>
+                <img src="./images/Counter.png" alt="dot">
+                <h4 class="info-icons-p2">2018</h4>
+            </div>
+            <div id="desktop-popup" class="popup-img1">
+                <img src="./images/popup desktop.png" alt="popup image 1">
+            </div>
+            <div id="mobile-popup" class="popup-img2">
+                <img src="./images/Snapshoot Portfolio.png" alt="popup image 1">
+            </div>
+            <div class="popup-text">
+                <div class="popup-body">
+                    <p class="desk-hidden">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea
+                    </p>
+                    <p class="mobile-hidden">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent
+                    </p>
+                </div>
+                <div id ="mobile-popup-langs" class="popup-lang">
+                    <div id="lang">
+                        <ul class="langs">
+                            <li>html</li>
+                            <li>css</li>
+                            <li>javascript</li>
+                            <li>github</li>
+                            <li>ruby</li>
+                            <li>bootstrap</li>
+                        </ul>
+                    </div>
+                    <div class="pop-button-div">
+                        <a href="#">
+                            <button type="submit" class="see-project pop-button">
+                            See Live
+                            <img src="./images/button Icon.png" alt="">
+                            </button>
+                        </a>
+                        <button type="submit" class="see-project pop-button">
+                            See Source
+                            <img src="./images/button Vector.png" alt="">
+                            </button>
+                        </a>
+                    </div>
+                    
+                </div>
+                <div id ="mobile-popup-langs"class="popup-langs">
+                    <div id="lang">
+                        <ul class="langs">
+                            <li>html</li>
+                            <li>css</li>
+                            <li>javascript</li>
+                        </ul>
+                    </div>
+                    <div class="pop-button-div">
+                        <a href="#">
+                            <button type="submit" class="see-project pop-button">
+                            See Live
+                            <img src="./images/button Icon.png" alt="">
+                            </button>
+                        </a>
+                        <button type="submit" class="see-project pop-button">
+                            See Source
+                            <img src="./images/button Vector.png" alt="">
+                            </button>
+                        </a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</div>`;
+
+    document.getElementById('popup-section').style.display = 'block';
+    document.getElementById('container').style.filter = 'blur(7px)';
+  
+}
+  
+function closePopup() {
+    document.getElementById('popup-section').style.display = 'none';
+    document.getElementById('container').style.filter = 'blur(0px)';
+}
